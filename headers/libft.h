82f15f840473aecf4362dffcba61ac420e8e4d1f
Dancode52 <daniellanehart@gmail.com>
2026-08-11 07:44:48 +0200

    Created basic strcmp and added to header and makefile

--- headers/libft.h
+++ headers/libft.h
@@ -6,7 +6,7 @@
 /*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 10:42:48 by dlanehar          #+#    #+#             */
-/*   Updated: 2026/03/09 10:32:11 by dlanehar         ###   ########.fr       */
+/*   Updated: 2026/08/11 07:39:17 by dlanehar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ int		ft_toupper(int c);
 int		ft_atoi(const char *str);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
---
+++ srcs/ft_strcmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlanehar <dlanehar@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 07:38:05 by dlanehar          #+#    #+#             */
+/*   Updated: 2026/08/11 07:42:13 by dlanehar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t					i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char) s1[i] - (unsigned char)s2[i]);
+}
